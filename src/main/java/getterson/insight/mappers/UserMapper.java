2ade7fb0c7408e589e0207e6db8629d7538d18a2
Math <math.lima.m@gmail.com>
2024-10-31 13:29:20 -0300

    refact: create UserMapper

--- src/main/java/getterson/insight/mappers/UserMapper.java
+++ src/main/java/getterson/insight/mappers/UserMapper.java
@@ -1,4 +1,11 @@
 package getterson.insight.mappers;
 
-public class UserMapper {
+import getterson.insight.dtos.UserDTO;
+import getterson.insight.entities.UserEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDTO userToUserDTO(UserEntity user);
+    UserEntity UserDTOToUserEntity(UserDTO userDTO);
 }
