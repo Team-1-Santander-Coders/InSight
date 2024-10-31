f38dfcb181e2238ce12474d1c05dc641ca45266b
Allana <allanac.avila@gmail.com>
2024-10-31 15:28:21 -0300

    criando interface summary repository

--- src/main/java/getterson/insight/repositories/SummaryRepository.java
+++ src/main/java/getterson/insight/repositories/SummaryRepository.java
@@ -1,4 +1,25 @@
 package getterson.insight.repositories;
 
-public interface SummaryRepository {
+
+import getterson.insight.entities.SummaryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@Repository
+public interface SummaryRepository extends JpaRepository<SummaryEntity, Long> {
+
+    //buscar por categoria específica
+    List<SummaryEntity> findByCategoriesContaining(String category);
+
+    //buscar por titulo do topico associado
+    List<SummaryEntity> findByTopicTitle(String topicTitle);
+
+
+    //ou com paginacao
+    Page<SummaryEntity> findByCategoriesContaining(String category, Pageable pageable);
+    Page<SummaryEntity> findByTopicTitle(String topicTitle, Pageable pageable);
 }
