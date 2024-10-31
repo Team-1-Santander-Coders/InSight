8e545b9493839c6418681e43cc7b0602a87bb2ce
Allana <allanac.avila@gmail.com>
2024-10-31 20:32:51 -0300

    alterando para interface e criando os atributos

--- src/main/java/getterson/insight/mappers/SummaryMapper.java
+++ src/main/java/getterson/insight/mappers/SummaryMapper.java
@@ -1,7 +1,15 @@
 package getterson.insight.mappers;
 
+import getterson.insight.dtos.SummaryDTO;
+import getterson.insight.entities.SummaryEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-public class SummaryMapper {
 
+@Mapper
+public interface SummaryMapper {
+    SummaryMapper INSTANCE = Mappers.getMapper(SummaryMapper.class);
+
+    SummaryDTO toDTO(SummaryEntity summaryEntity);
+    SummaryEntity toEntity(SummaryDTO summaryDTO);
 }
