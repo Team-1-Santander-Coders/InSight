047c97db2ad518ffed0606084bdc749465b8f9ac
Math <math.lima.m@gmail.com>
2024-10-31 13:28:47 -0300

    refact: clean code

--- src/main/java/getterson/insight/mappers/SummaryDataMapper.java
+++ src/main/java/getterson/insight/mappers/SummaryDataMapper.java
@@ -3,12 +3,9 @@ package getterson.insight.mappers;
 import getterson.insight.dtos.SummaryDataDTO;
 import getterson.insight.entities.SummaryDataEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface SummaryDataMapper {
-    //SummaryDataMapper INSTANCE = Mappers.getMapper(SummaryDataMapper.class);
-
     SummaryDataDTO toDTO(SummaryDataEntity entity);
     SummaryDataEntity toEntity(SummaryDataDTO dto);
 }
