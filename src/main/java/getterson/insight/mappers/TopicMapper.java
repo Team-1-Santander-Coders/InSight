d190bb535864feccd69b563a633872749a53f69e
Math <math.lima.m@gmail.com>
2024-11-04 21:23:00 -0300

    refact: update TopicMapper

--- src/main/java/getterson/insight/mappers/TopicMapper.java
+++ src/main/java/getterson/insight/mappers/TopicMapper.java
@@ -1,13 +1,32 @@
 package getterson.insight.mappers;
 
+import getterson.insight.dtos.SummaryDTO;
 import getterson.insight.dtos.TopicDTO;
 import getterson.insight.entities.TopicEntity;
 
-import org.mapstruct.Mapper;
+import getterson.insight.mappers.Mapper;
+import getterson.insight.services.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface TopicMapper {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-    TopicDTO toDTO(TopicEntity topicEntity);
-    TopicEntity toEntity (TopicDTO topicDTO);
+@Component
+public class TopicMapper implements Mapper<TopicEntity, TopicDTO> {
+
+    @Autowired
+    private  SummaryMapper summaryMapper;
+
+    @Autowired
+    private TopicService topicService;
+
+    public TopicDTO toDTO(TopicEntity topicEntity){
+        return new TopicDTO(topicEntity.getId(), topicEntity.getTitle(), summaryMapper.toDTO(topicEntity.getSummaries()));
+    }
+
+    public TopicEntity toEntity (TopicDTO topicDTO) throws Exception {
+        return topicService.findById(topicDTO.id());
+    }
 }
