41ca72bdb4e4ec1048c6f43fd17661050f13154e
Math <math.lima.m@gmail.com>
2024-11-04 21:30:02 -0300

    refact: update SummaryMapper

--- src/main/java/getterson/insight/mappers/SummaryMapper.java
+++ src/main/java/getterson/insight/mappers/SummaryMapper.java
@@ -1,4 +1,32 @@
 package getterson.insight.mappers;
 
-public class SummaryMapper {
+import getterson.insight.dtos.SummaryDTO;
+
+import getterson.insight.entities.SummaryEntity;
+import getterson.insight.services.SummaryDataService;
+import getterson.insight.services.SummaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class SummaryMapper implements Mapper<SummaryEntity, SummaryDTO> {
+
+    @Autowired
+    private SummaryDataMapper summaryDataMapper;
+    @Autowired
+    private SummaryDataService summaryDataService;
+
+    @Autowired
+    private SummaryService summaryService;
+
+    public SummaryDTO toDTO(SummaryEntity summaryEntity){
+        return new SummaryDTO(summaryEntity.getId(), summaryEntity.getCategories(), summaryDataMapper.toDTO(summaryEntity.getSummaryDataList()), summaryEntity.getTopic().getTitle());
+    };
+
+    public SummaryEntity toEntity(SummaryDTO summaryDTO) throws Exception {
+        return summaryService.findSummaryById(summaryDTO.id());
+    }
 }
+
