cb29314909a3eea73bdfd7b000f4e74787d03857
Math <math.lima.m@gmail.com>
2024-11-04 21:14:12 -0300

    refact: update UserPreferenceDTO

--- src/main/java/getterson/insight/dtos/UserPreferenceDTO.java
+++ src/main/java/getterson/insight/dtos/UserPreferenceDTO.java
@@ -4,9 +4,7 @@ import getterson.insight.entities.types.PreferenceType;
 
 import java.util.List;
 
-public record UserPreferenceDTO(
-        long id,
-        long userId,
-        List<String> blackListWords,
-        PreferenceType type
-) {}
+public record UserPreferenceDTO(long id,
+                                long userId,
+                                List<String> blackListWords,
+                                PreferenceType type) {}
