0058f8d4cff9d81b58d898cf76695d82a2b9f5d5
Math <math.lima.m@gmail.com>
2024-11-05 22:13:31 -0300

    feat: add SummaryDataRepository coded by Allana

--- src/main/java/getterson/insight/repositories/SummaryDataRepository.java
+++ src/main/java/getterson/insight/repositories/SummaryDataRepository.java
@@ -1,6 +1,17 @@
 package getterson.insight.repositories;
 
 import getterson.insight.entities.SummaryDataEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SummaryDataRepository extends JpaRepository <SummaryDataEntity, Long> {}
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SummaryDataRepository extends JpaRepository <SummaryDataEntity, Long> {
+    List<SummaryDataEntity> findByAbout(String about);
+    List<SummaryDataEntity> findByDate(LocalDate date);
+
+    Page<SummaryDataEntity> findByAbout(String about, Pageable pageable);
+    Page<SummaryDataEntity> findByDate(LocalDate date, Pageable pageable);
+}
