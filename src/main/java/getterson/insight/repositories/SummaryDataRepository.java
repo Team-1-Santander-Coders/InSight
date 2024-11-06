8ea7478d6efe4b395145fe11ecfa371a8491eeb5
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-11-05 22:04:22 -0300

    fix: fix summary data repository

--- src/main/java/getterson/insight/repositories/SummaryDataRepository.java
+++ src/main/java/getterson/insight/repositories/SummaryDataRepository.java
@@ -1,4 +1,17 @@
 package getterson.insight.repositories;
 
-public interface SummaryDataRepository {
+import getterson.insight.entities.SummaryDataEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SummaryDataRepository extends JpaRepository<SummaryDataEntity, Long> {
+    List<SummaryDataEntity> findByAbout(String about);
+    List<SummaryDataEntity> findByDate(LocalDate date);
+
+    Page<SummaryDataEntity> findByAbout(String about, Pageable pageable);
+    Page<SummaryDataEntity> findByDate(LocalDate date, Pageable pageable);
 }
--- src/main/java/getterson/insight/services/SummaryDataService.java
+++ src/main/java/getterson/insight/services/SummaryDataService.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Service
 public class SummaryDataService {
 
+
     private final SummaryDataRepository summaryDataRepository;
 
     @Autowired
