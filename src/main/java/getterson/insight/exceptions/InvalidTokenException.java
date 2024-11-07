504363bd35d8ce41e0ac3e75f83bdecfdf9a6083
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-11-07 17:07:59 -0300

    feat: implement and improve JWTService

--- src/main/java/getterson/insight/exceptions/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package getterson.insight.exceptions;
-
-public class InvalidTokenException extends Exception {
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-}
--- src/main/java/getterson/insight/services/JWTService.java
+++ src/main/java/getterson/insight/services/JWTService.java
@@ -1,40 +1,89 @@
 package getterson.insight.services;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import getterson.insight.dtos.TokenResult;
 import getterson.insight.entities.TokenEntity;
-import getterson.insight.exceptions.InvalidTokenException;
+import getterson.insight.entities.UserEntity;
+import getterson.insight.exceptions.AuthenticationFailedException;
 import getterson.insight.repositories.TokenRepository;
-import getterson.insight.repositories.UserRepository;
+import static getterson.insight.utils.DateUtil.ZONEOFFSET;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Optional;
 
 @Service
 public class JWTService {
+
+    @Value("${my.secretkey}")
+    private String secretKey;
+
     private final TokenRepository tokenRepository;
+    private final String ISSUER = "InSight";
+    private final Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
     public JWTService(TokenRepository tokenRepository) {
         this.tokenRepository = tokenRepository;
     }
 
-    public TokenEntity save(TokenEntity tokenEntity) {
-        return tokenRepository.saveAndFlush(tokenEntity);
-    }
-
-    public TokenResult getToken(String token) throws InvalidTokenException {
+    public Optional<TokenResult> getToken(String token) {
         Optional<TokenEntity> tokenEntityOptional = tokenRepository.findByToken(token);
 
         if (tokenEntityOptional.isPresent()) {
             TokenEntity tokenEntity = tokenEntityOptional.get();
-            return new TokenResult(tokenEntity, isExpired(tokenEntity));
+            return Optional.of(new TokenResult(tokenEntity, isExpired(tokenEntity)));
         }
 
-        throw new InvalidTokenException("Token inválido.");
+        return Optional.empty();
     }
 
     public boolean isExpired(TokenEntity tokenEntity) {
         return tokenEntity.getExpirationDate().isBefore(Instant.now());
+    }
+
+    public String generateToken(UserEntity user) throws AuthenticationFailedException {
+        try {
+            Instant expirationDate = generateExpirationDate();
+            String token = JWT.create()
+                    .withIssuer(ISSUER)
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(expirationDate)
+                    .sign(algorithm);
+
+            TokenEntity tokenEntity = new TokenEntity(token, expirationDate, user);
+            tokenRepository.saveAndFlush(tokenEntity);
+            return token;
+        } catch (JWTCreationException e) {
+            throw new AuthenticationFailedException("Erro durante a autenticação.");
+        }
+    }
+
+    public Optional<String> validateToken(String token) {
+        try {
+            Optional<TokenResult> tokenResultOpt = getToken(token);
+            if (tokenResultOpt.isPresent()) {
+                TokenResult tokenResult = tokenResultOpt.get();
+                if (tokenResult.isExpired()) return Optional.empty();
+                return Optional.ofNullable(JWT.require(algorithm)
+                        .withIssuer(ISSUER)
+                        .build()
+                        .verify(token)
+                        .getSubject());
+            }
+
+            return Optional.empty();
+        } catch (JWTCreationException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusDays(7).toInstant(ZoneOffset.of(ZONEOFFSET));
     }
 }
