676c5db4779a6ec0d7c71c7925a85abea34c7754
Math <math.lima.m@gmail.com>
2024-11-11 14:59:42 -0300

    feat: create findByTopicEntityAndUserEntity method

--- src/main/java/getterson/insight/repositories/TopicPreferenceRepository.java
+++ src/main/java/getterson/insight/repositories/TopicPreferenceRepository.java
@@ -1,11 +1,14 @@
 package getterson.insight.repositories;
 
 import getterson.insight.dtos.TopicPreferenceDTO;
+import getterson.insight.entities.TopicEntity;
 import getterson.insight.entities.TopicPreferenceEntity;
+import getterson.insight.entities.UserEntity;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TopicPreferenceRepository extends JpaRepository<TopicPreferenceEntity, Long> {
 
@@ -15,4 +18,10 @@ public interface TopicPreferenceRepository extends JpaRepository<TopicPreference
     List<TopicPreferenceDTO> findAllByUserIdAsDTO(
             @Param("userId") long userId
     );
+
+
+    @Query("SELECT tp FROM TopicPreferenceEntity tp WHERE tp.topicEntity = :topicEntity AND tp.user = :userEntity")
+    Optional<TopicPreferenceEntity> findByTopicEntityAndUserEntity(@Param("topicEntity") TopicEntity topicEntity, @Param("userEntity") UserEntity userEntity);
+
+
 }
